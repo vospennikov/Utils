63ab4c843056fe78b93ecbec0f0103f6637290d5
Mikhail Vospennikov <m.vospennikov@gmail.com>
2023-05-05 20:19:04 +0400

    Reformat code

--- clean_gems.sh
+++ clean_gems.sh
@@ -1,22 +1,20 @@
 #!/usr/bin/env bash
 
 uninstall() {
-  list=`gem list --no-versions`
-  for gem in $list; do
-    gem uninstall $gem -aIx
-  done
-  gem list
-  gem install bundler
+    list=$(gem list --no-versions)
+    for gem in $list; do
+        gem uninstall "$gem" -aIx
+    done
+    gem list
+    gem install bundler
 }
 
-#rbenv versions --bare
-RBENVPATH=`rbenv root`
-echo $RBENVPATH
-RUBIES=`ls $RBENVPATH/versions`
+RBENVPATH=$(rbenv root)
+echo "$RBENVPATH"
+RUBIES=$(ls "$RBENVPATH"/versions)
 for ruby in $RUBIES; do
-  echo '---------------------------------------'
-  echo $ruby
-  rbenv local $ruby
-  uninstall
+    echo '---------------------------------------'
+    echo "$ruby"
+    rbenv local "$ruby"
+    uninstall
 done
-
